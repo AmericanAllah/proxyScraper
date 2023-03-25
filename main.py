86fa545783f3012a7a151a4ec90e101198dad63e
AmericanAllah <31081324+AmericanAllah@users.noreply.github.com>
2023-03-25 18:48:40 -0400

    multithreading

--- main.py
+++ main.py
@@ -1,98 +1,125 @@
+import sys
 import requests
-import time
+import json
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+import concurrent.futures
 
-def fetch_proxy_data():
+def fetch_proxies():
+    print("Fetching proxies...")
     url = "https://proxylist.geonode.com/api/proxy-list?limit=500&page=1&sort_by=lastChecked&sort_type=desc"
     response = requests.get(url)
     data = response.json()
     return data["data"]
 
-def get_proxy_location(proxy_ip):
-    url = f"http://ip-api.com/json/{proxy_ip}"
-    response = requests.get(url)
-    data = response.json()
-
-    if data["status"] == "success":
-        return {
-            "country": data["country"],
-            "region": data["regionName"],
-            "city": data["city"]
-        }
-    else:
-        return None
-
 def test_proxy_speed(proxy):
-    test_url = "http://httpbin.org/get"
+    test_url = "https://httpbin.org/ip"
     proxies = {
-        "http": f"http://{proxy['ip']}:{proxy['port']}",
-        "https": f"http://{proxy['ip']}:{proxy['port']}"
+        "http": f"{proxy['ip']}:{proxy['port']}",
+        "https": f"{proxy['ip']}:{proxy['port']}",
     }
+    session = requests.Session()
+    retry = Retry(total=3, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
 
     try:
-        start_time = time.time()
-        response = requests.get(test_url, proxies=proxies, timeout=10)
-        response.raise_for_status()
-        elapsed_time = time.time() - start_time
+        response = session.get(test_url, proxies=proxies, timeout=5)
+        if response.status_code == 200:
+            return response.elapsed.total_seconds()
     except requests.exceptions.RequestException:
-        elapsed_time = None
+        pass
 
-    return elapsed_time
+    return None
+
+def get_proxy_location(ip_address):
+    location_url = f"https://ipapi.co/{ip_address}/json/"
+    response = requests.get(location_url)
+    location_data = response.json()
+    return {
+        "country": location_data.get("country_name"),
+        "region": location_data.get("region"),
+        "city": location_data.get("city"),
+    }
+
+def test_and_save_proxy(proxy, total_proxies, index):
+    response_time = test_proxy_speed(proxy)
+    print(f"Testing proxy {index + 1}/{total_proxies}: {proxy['ip']}:{proxy['port']}")
+
+    if response_time is not None:
+        proxy["response_time"] = response_time
+        proxy["location"] = get_proxy_location(proxy["ip"])
+        print(f"Proxy {proxy['ip']}:{proxy['port']} works! (Response Time: {response_time} seconds)")
+
+        with open("GOODPROXIES.txt", "a") as good_proxies_file:
+            good_proxies_file.write(f"{proxy['ip']}:{proxy['port']}\n")
+
+        return proxy
+
+    return None
 
 def filter_proxies(proxies, filter_type=None, value=None):
     print("Filtering proxies...")
 
     filtered_proxies = []
     total_proxies = len(proxies)
-    
-    with open("GOODPROXIES.txt", "w") as good_proxies_file:
-        for index, proxy in enumerate(proxies):
-            response_time = test_proxy_speed(proxy)
-            print(f"Testing proxy {index + 1}/{total_proxies}: {proxy['ip']}:{proxy['port']}")
 
-            if response_time is not None:
-                proxy["response_time"] = response_time
-                proxy["location"] = get_proxy_location(proxy["ip"])
-                filtered_proxies.append(proxy)
-                print(f"Proxy {proxy['ip']}:{proxy['port']} works! (Response Time: {response_time} seconds)")
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(test_and_save_proxy, proxy, total_proxies, index) for index, proxy in enumerate(proxies)]
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            if result is not None:
+                filtered_proxies.append(result)
 
-                good_proxies_file.write(f"{proxy['ip']}:{proxy['port']}\n")
+    if filter_type == '%':
+        percentage = int(value)
+        filtered_proxies = sorted(filtered_proxies, key=lambda x: x["response_time"])
+        top_proxies = int(len(filtered_proxies) * percentage / 100)
+        filtered_proxies = filtered_proxies[:top_proxies]
+    elif filter_type == '#':
+        number = int(value)
+        filtered_proxies = sorted(filtered_proxies, key=lambda x: x["response_time"])
+        filtered_proxies = filtered_proxies[:number]
 
-    # TODO: Implement filtering based on filter_type and value
-    # For now, just printing the filtered_proxies
     print("Finished filtering proxies.")
     print(filtered_proxies)
 
-def find_proxies():
-    print('Finding proxies...')
-    proxy_list = fetch_proxy_data()
-    filter_proxies(proxy_list)
 
 def main():
-    print('1. Find and test proxies, keep working proxies')
-    print('2. Find and test proxies, keep top % or # of working proxies')
-    print('3. Test proxies in PROXIES.txt')
-    print('4. Exit')
-    option = input('Enter option: ')
+    while True:
+        print("1. Find and test proxies, save working proxies")
+        print("2. Find and test proxies, save top % or # of working proxies")
+        print("3. Test proxies in PROXIES.txt")
+        print("4. Exit")
 
-    if option == '1':
-        find_proxies()
-    elif option == '2':
-        print('1. Find and test proxies, keep fastest % of working proxies')
-        print('2. Find and test proxies, keep fastest # of working proxies')
-        option = input('Enter option: ')
-        if option == '1':
-            input_value = input('Enter %: ')
-            find_proxies()
-            filter_proxies(filter_type='%', value=input_value)
-        elif option == '2':
-            input_value = input('Enter #: ')
-            find_proxies()
-            filter_proxies(filter_type='#', value=input_value)
-    elif option == '3':
-        filter_proxies()
-    else:
-        print('Exiting...')
-        sys.exit()
+        option = input("Enter option: ")
 
-if __name__ == '__main__':
+        if option == "1":
+            proxies = fetch_proxies()
+            filter_proxies(proxies)
+        elif option == "2":
+            print("1. Find and test proxies, keep fastest % of working proxies")
+            print("2. Find and test proxies, keep fastest # of working proxies")
+            filter_option = input("Enter option: ")
+
+            if filter_option == "1":
+                percentage = input("Enter %: ")
+                proxies = fetch_proxies()
+                filter_proxies(proxies, filter_type="%", value=percentage)
+            elif filter_option == "2":
+                number = input("Enter #: ")
+                proxies = fetch_proxies()
+                filter_proxies(proxies, filter_type="#", value=number)
+        elif option == "3":
+            with open("PROXIES.txt", "r") as proxies_file:
+                proxies = [line.strip() for line in proxies_file.readlines()]
+            filter_proxies(proxies)
+        elif option == "4":
+            print("Exiting...")
+            sys.exit()
+        else:
+            print("Invalid option. Please try again.")
+
+if __name__ == "__main__":
     main()
